60517bd8d7b37c52e6c9eb6efe5659fd271b940c
丨一 <leung2014@qq.com>
2025-12-25 17:38:45 +0800

    基础的重命名功能

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
 )
 
 func main() {
@@ -10,12 +11,30 @@ func main() {
 }
 
 func doCommand(args []string) {
-	if len(args) == 0 || args[0] == "-h" {
+	if len(args) < 2 || args[0] == "-h" {
 		showHelp()
 		return
+	}
+
+	//源文件名 -> 可能会包含路径
+	from := args[0]
+
+	//目标名
+	to := args[1]
+
+	//源文件目录
+	dir := path.Dir(from)
+
+	newName := dir + "/" + to
+
+	err := os.Rename(from, newName)
+
+	if err != nil {
+		fmt.Println("Err:", err)
+		os.Exit(1)
 	}
 }
 
 func showHelp() {
-	fmt.Println("this is help")
+	fmt.Println("Usage: $ rename [from] [to] [options]")
 }
